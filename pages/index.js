b9a0b5dfd5ca401d4a1769f99afadf0a697fcbde
Fatima G. Lazan <33022083+fatimalazan@users.noreply.github.com>
2021-12-16 23:39:40 +0800

    Update

--- pages/index.js
+++ pages/index.js
@@ -51,10 +51,10 @@ export default function Home() {
     <>
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" type="image/png" href="/favicon-32x32.png" />
         <title>Frontend Mentor | Tip Calculator App</title>
