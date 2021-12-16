66a2b5475a48d8670bd6bce10dc6c7ad54748b32
Fatima G. Lazan <33022083+fatimalazan@users.noreply.github.com>
2021-12-17 01:09:37 +0800

    Update

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,6 @@ export default function Home() {
     <>
       <Head>
         <meta charset="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
